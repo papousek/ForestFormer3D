d781e15f36b01e06b0d526725d3b449a8b1b2f84
Jan Papousek <jan.papousek@flowerchecker.com>
2026-02-18 13:47:39 +0100

    style: black formatting

--- data/ForAINetV2/load_forainetv2_data.py
+++ data/ForAINetV2/load_forainetv2_data.py
@@ -14,8 +14,7 @@ import os
 import numpy as np
 from plyutils import read_ply
 
-currentdir = os.path.dirname(
-    os.path.abspath(inspect.getfile(inspect.currentframe())))
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
 
 def read_aggregation(filename):
@@ -24,12 +23,13 @@ def read_aggregation(filename):
     label_to_segs = {}
     with open(filename) as f:
         data = json.load(f)
-        num_objects = len(data['segGroups'])
+        num_objects = len(data["segGroups"])
         for i in range(num_objects):
-            object_id = data['segGroups'][i][
-                'objectId'] + 1  # instance ids should be 1-indexed
-            label = data['segGroups'][i]['label']
-            segs = data['segGroups'][i]['segments']
+            object_id = (
+                data["segGroups"][i]["objectId"] + 1
+            )  # instance ids should be 1-indexed
+            label = data["segGroups"][i]["label"]
+            segs = data["segGroups"][i]["segments"]
             object_id_to_segs[object_id] = segs
             if label in label_to_segs:
                 label_to_segs[label].extend(segs)
@@ -43,9 +43,9 @@ def read_segmentation(filename):
     seg_to_verts = {}
     with open(filename) as f:
         data = json.load(f)
-        num_verts = len(data['segIndices'])
+        num_verts = len(data["segIndices"])
         for i in range(num_verts):
-            seg_id = data['segIndices'][i]
+            seg_id = data["segIndices"][i]
             if seg_id in seg_to_verts:
                 seg_to_verts[seg_id].append(i)
             else:
@@ -89,9 +89,7 @@ def extract_bbox(mesh_vertices, label_ids, instance_ids, bg_sem=np.array([0])):
     return instance_bboxes
 
 
-def export(ply_file,
-           output_file=None,
-           test_mode=False):
+def export(ply_file, output_file=None, test_mode=False):
     """Export original files to vert, ins_label, sem_label and bbox file.
 
     Args:
@@ -109,19 +107,19 @@ def export(ply_file,
         dict: Map from object_id to label_id.
     """
 
-    #from plyfile import PlyData, PlyElement
-    #def read_ply(filename):
+    # from plyfile import PlyData, PlyElement
+    # def read_ply(filename):
     #    """Read a PLY file and return its contents as a dictionary."""
     #    ply_data = PlyData.read(filename)
     #    data = ply_data['vertex'].data
     #    return {key: data[key] for key in data.dtype.names}
 
     pcd = read_ply(ply_file)
-    #points = np.vstack((pcd['x'], pcd['y'], pcd['z'])).astype(np.float32).T
+    # points = np.vstack((pcd['x'], pcd['y'], pcd['z'])).astype(np.float32).T
 
-    points = np.vstack((pcd['x'], pcd['y'], pcd['z'])).astype(np.float64).T
+    points = np.vstack((pcd["x"], pcd["y"], pcd["z"])).astype(np.float64).T
 
-    is_blue = 'bluepoints' in os.path.basename(ply_file)
+    is_blue = "bluepoints" in os.path.basename(ply_file)
 
     if is_blue:
         offsets = np.zeros(3, dtype=np.float64)
@@ -139,12 +137,12 @@ def export(ply_file,
 
     points = points.astype(np.float32)
 
-    #semantic_seg = np.ones((points.shape[0],), dtype=np.int64)
-    #treeID = np.zeros((points.shape[0],), dtype=np.int64)
+    # semantic_seg = np.ones((points.shape[0],), dtype=np.int64)
+    # treeID = np.zeros((points.shape[0],), dtype=np.int64)
     semantic_seg = pcd["semantic_seg"].astype(np.int64)
     treeID = pcd["treeID"].astype(np.int64)
-    #semantic_seg = pcd["semantic"].astype(np.int64)
-    #treeID = pcd["instance"].astype(np.int64)
+    # semantic_seg = pcd["semantic"].astype(np.int64)
+    # treeID = pcd["instance"].astype(np.int64)
 
     # test set data doesn't have align_matrix
     axis_align_matrix = np.eye(4)
@@ -159,15 +157,14 @@ def export(ply_file,
     # Load semantic and instance labels
     if not test_mode:
         # semantic label
-        bg_sem=np.array([0])   #####wythan wood np.array([1])
+        bg_sem = np.array([0])  #####wythan wood np.array([1])
         label_ids = semantic_seg - 1
         instance_ids = treeID  # 0: unannotated
 
         # Set instance_ids of background points to -1
         instance_ids[np.isin(label_ids, bg_sem)] = -1
-    
 
-        '''
+        """
         # Get unique instance IDs that are not -1
         unique_instance_ids = np.unique(instance_ids[instance_ids != -1])
     
@@ -181,7 +178,7 @@ def export(ply_file,
         # Set background points back to 0
         new_instance_ids[instance_ids == -1] = 0
         instance_ids = new_instance_ids
-        '''
+        """
         # Create a mask for non-background points
         valid_mask = instance_ids != -1
 
@@ -198,7 +195,9 @@ def export(ply_file,
         instance_ids = new_instance_ids
 
         unaligned_bboxes = extract_bbox(points, label_ids, instance_ids, bg_sem)
-        aligned_bboxes = extract_bbox(aligned_mesh_vertices, label_ids, instance_ids, bg_sem)
+        aligned_bboxes = extract_bbox(
+            aligned_mesh_vertices, label_ids, instance_ids, bg_sem
+        )
     else:
         label_ids = None
         instance_ids = None
@@ -206,47 +205,59 @@ def export(ply_file,
         aligned_bboxes = None
 
     if output_file is not None:
-        np.save(output_file + '_vert.npy', points)
+        np.save(output_file + "_vert.npy", points)
         if not test_mode:
-            np.save(output_file + '_sem_label.npy', label_ids)
-            np.save(output_file + '_ins_label.npy', instance_ids)
-            np.save(output_file + '_unaligned_bbox.npy', unaligned_bboxes)
-            np.save(output_file + '_aligned_bbox.npy', aligned_bboxes)
-            np.save(output_file + '_axis_align_matrix.npy', axis_align_matrix)
+            np.save(output_file + "_sem_label.npy", label_ids)
+            np.save(output_file + "_ins_label.npy", instance_ids)
+            np.save(output_file + "_unaligned_bbox.npy", unaligned_bboxes)
+            np.save(output_file + "_aligned_bbox.npy", aligned_bboxes)
+            np.save(output_file + "_axis_align_matrix.npy", axis_align_matrix)
 
-    return points, label_ids, instance_ids, unaligned_bboxes, \
-        aligned_bboxes, axis_align_matrix, offsets
+    return (
+        points,
+        label_ids,
+        instance_ids,
+        unaligned_bboxes,
+        aligned_bboxes,
+        axis_align_matrix,
+        offsets,
+    )
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--scan_path',
+        "--scan_path",
         required=True,
-        help='path to scannet scene (e.g., data/ScanNet/v2/scene0000_00')
-    parser.add_argument('--output_file', required=True, help='output file')
+        help="path to scannet scene (e.g., data/ScanNet/v2/scene0000_00",
+    )
+    parser.add_argument("--output_file", required=True, help="output file")
     parser.add_argument(
-        '--label_map_file',
-        required=True,
-        help='path to scannetv2-labels.combined.tsv')
+        "--label_map_file", required=True, help="path to scannetv2-labels.combined.tsv"
+    )
     parser.add_argument(
-        '--scannet200',
-        action='store_true',
-        help='Use it for scannet200 mapping')
+        "--scannet200", action="store_true", help="Use it for scannet200 mapping"
+    )
 
     opt = parser.parse_args()
 
     scan_name = os.path.split(opt.scan_path)[-1]
-    mesh_file = os.path.join(opt.scan_path, scan_name + '_vh_clean_2.ply')
-    agg_file = os.path.join(opt.scan_path, scan_name + '.aggregation.json')
-    seg_file = os.path.join(opt.scan_path,
-                            scan_name + '_vh_clean_2.0.010000.segs.json')
+    mesh_file = os.path.join(opt.scan_path, scan_name + "_vh_clean_2.ply")
+    agg_file = os.path.join(opt.scan_path, scan_name + ".aggregation.json")
+    seg_file = os.path.join(opt.scan_path, scan_name + "_vh_clean_2.0.010000.segs.json")
     meta_file = os.path.join(
-        opt.scan_path, scan_name +
-        '.txt')  # includes axisAlignment info for the train set scans.
-    export(mesh_file, agg_file, seg_file, meta_file, opt.label_map_file,
-           opt.output_file, scannet200=opt.scannet200)
+        opt.scan_path, scan_name + ".txt"
+    )  # includes axisAlignment info for the train set scans.
+    export(
+        mesh_file,
+        agg_file,
+        seg_file,
+        meta_file,
+        opt.label_map_file,
+        opt.output_file,
+        scannet200=opt.scannet200,
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
