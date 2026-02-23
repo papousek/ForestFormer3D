d19e2e92c93857b86330737f78e9ef067ab24eb2
Jan Papousek <jan.papousek@flowerchecker.com>
2026-02-23 13:33:52 +0100

    style: black formatting

--- tools/create_data_forainetv2.py
+++ tools/create_data_forainetv2.py
@@ -15,43 +15,47 @@ def forainetv2_data_prep(root_path, info_prefix, out_dir, workers):
         out_dir (str): Output directory of the generated info file.
         workers (int): Number of threads to be used.
     """
-    create_info_file(
-        root_path, info_prefix, out_dir, workers=workers)
-    info_train_path = osp.join(out_dir, f'{info_prefix}_oneformer3d_infos_train.pkl')
-    info_val_path = osp.join(out_dir, f'{info_prefix}_oneformer3d_infos_val.pkl')
-    info_test_path = osp.join(out_dir, f'{info_prefix}_oneformer3d_infos_test.pkl')
+    create_info_file(root_path, info_prefix, out_dir, workers=workers)
+    info_train_path = osp.join(out_dir, f"{info_prefix}_oneformer3d_infos_train.pkl")
+    info_val_path = osp.join(out_dir, f"{info_prefix}_oneformer3d_infos_val.pkl")
+    info_test_path = osp.join(out_dir, f"{info_prefix}_oneformer3d_infos_test.pkl")
     update_pkl_infos(info_prefix, out_dir=out_dir, pkl_path=info_train_path)
     update_pkl_infos(info_prefix, out_dir=out_dir, pkl_path=info_val_path)
     update_pkl_infos(info_prefix, out_dir=out_dir, pkl_path=info_test_path)
 
 
-parser = argparse.ArgumentParser(description='Data converter arg parser')
-parser.add_argument('dataset', metavar='forainetv2', help='name of the dataset')
+parser = argparse.ArgumentParser(description="Data converter arg parser")
+parser.add_argument("dataset", metavar="forainetv2", help="name of the dataset")
 parser.add_argument(
-    '--root-path',
+    "--root-path",
     type=str,
-    default='./data/ForAINetV2',
-    help='specify the root path of dataset')
+    default="./data/ForAINetV2",
+    help="specify the root path of dataset",
+)
 parser.add_argument(
-    '--out-dir',
+    "--out-dir",
     type=str,
-    default='./data/ForAINetV2',
+    default="./data/ForAINetV2",
     required=False,
-    help='name of info pkl')
-parser.add_argument('--extra-tag', type=str, default='forainetv2')
+    help="name of info pkl",
+)
+parser.add_argument("--extra-tag", type=str, default="forainetv2")
 parser.add_argument(
-    '--workers', type=int, default=4, help='number of threads to be used')
+    "--workers", type=int, default=4, help="number of threads to be used"
+)
 args = parser.parse_args()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from mmdet3d.utils import register_all_modules
+
     register_all_modules()
 
-    if args.dataset in ('forainetv2'):
+    if args.dataset in ("forainetv2"):
         forainetv2_data_prep(
             root_path=args.root_path,
             info_prefix=args.extra_tag,
             out_dir=args.out_dir,
-            workers=args.workers)
+            workers=args.workers,
+        )
     else:
-        raise NotImplementedError(f'Don\'t support {args.dataset} dataset.')
+        raise NotImplementedError(f"Don't support {args.dataset} dataset.")
